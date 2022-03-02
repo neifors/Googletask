7de93c09cd9b07f48acf3f095cc992b350d2ab20
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-03-02 14:15:30 +0000

    added a random result function to controllers

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
+const searchData = require('../data')
 
 const Result = require('../models/models')
 
-router.use(express.static('google-search'));
-
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../google-search', 'index.html'));
+    res.send(searchData.searchList)
 });
 
 router.get('/:keyword', (req, res) => {
@@ -15,6 +13,18 @@ router.get('/:keyword', (req, res) => {
         const searchKeyword = req.params.keyword;
         const searchedItem = Result.findByKeyword(searchKeyword)
         res.send(searchedItem)
+    } catch (err) {
+        res.status(404)
+        res.send(err.message)
+    }
+});
+
+router.get('/:keyword/random', (req, res) => {
+    try {
+        const searchKeyword = req.params.keyword;
+        const searchedItem = Result.findByKeyword(searchKeyword)
+        const randomInt = Math.floor(Math.random() * searchedItem.length)
+        res.send(searchedItem[randomInt])
     } catch (err) {
         res.status(404)
         res.send(err.message)
